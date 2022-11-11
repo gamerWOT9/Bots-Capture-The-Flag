85b88ffa3c80d63488c4ed9e0f050539430a8922
gamerWOT9 <sulzynski.olivier@gmail.com>
2022-11-11 19:40:58 +0100

    BETA V0.20
    -header animation

--- static/js/script.js
+++ static/js/script.js
@@ -2,10 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementsByClassName("section1-navigation").style.padding = "30px 10px";
-    document.getElementsByClassName("section1-logo").style.fontSize = "25px";
+    document.getElementById("section1-idnav").style.padding = "0 var(--responsivePadding)";
+    document.getElementById("section1-idlogo").style.fontSize = "25px";
   } else {
-    document.getElementsByClassName("section1-navigation").style.padding = "80px 10px";
-    document.getElementsByClassName("section1-logo").style.fontSize = "35px";
+    document.getElementById("section1-idnav").style.padding = "80px 150px";
+    document.getElementById("section1-idlogo").style.fontSize = "35px";
   }
 }
