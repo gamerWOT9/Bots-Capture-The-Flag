d6e199b1645cb1c9301b0e9b8c08d7914aa90d26
gamerWOT9 <sulzynski.olivier@gmail.com>
2022-11-20 16:34:27 +0100

    BETA V0.44

--- static/js/script.js
+++ static/js/script.js
@@ -7,11 +7,13 @@ function scrollFunction() {
     document.getElementById("sb1-idbg").style.display = "none";
     document.getElementById("section1-idlinks").style.display = "flex";
     document.getElementById("sb1-idimglogo").style.height = "50px";
+    document.getElementById("sb1-idimgemoji").style.display = "none";
   } else {
     document.getElementById("section1-idnav").style.padding = "80px 150px";
     document.getElementById("section1-idlogo").style.margin = "auto";
     document.getElementById("sb1-idbg").style.display = "block";
     document.getElementById("section1-idlinks").style.display = "none";
     document.getElementById("sb1-idimglogo").style.height = "150px";
+    document.getElementById("sb1-idimgemoji").style.display = "flex";
   }
 }
