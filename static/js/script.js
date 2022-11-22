035bb68ccfceabaa7baa08e0f68ce08e441ec17e
gamerWOT9 <sulzynski.olivier@gmail.com>
2022-11-22 20:59:14 +0100

    BETA V0.50

--- static/js/script.js
+++ static/js/script.js
@@ -5,15 +5,15 @@ function scrollFunction() {
     document.getElementById("section1-idnav").style.padding = "0 var(--responsivePadding)";
     document.getElementById("section1-idlogo").style.margin = "1px";
     document.getElementById("sb1-idbg").style.display = "none";
-    document.getElementById("section1-idlinks").style.display = "flex";
+    document.getElementById("section1-idlinks").style.translate = "0px";
+    document.getElementById("section1-idlinks").style.opacity = "1";
     document.getElementById("sb1-idimglogo").style.height = "50px";
-    document.getElementById("sb1-idimgemoji").style.display = "none";
   } else {
     document.getElementById("section1-idnav").style.padding = "80px 150px";
     document.getElementById("section1-idlogo").style.margin = "auto";
     document.getElementById("sb1-idbg").style.display = "block";
-    document.getElementById("section1-idlinks").style.display = "none";
+    document.getElementById("section1-idlinks").style.translate = "20px 50px";
+    document.getElementById("section1-idlinks").style.opacity = "0";
     document.getElementById("sb1-idimglogo").style.height = "150px";
-    document.getElementById("sb1-idimgemoji").style.display = "flex";
   }
 }
