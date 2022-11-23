c68a72bba4da51c41b35aa604672ee9561ab0d05
gamerWOT9 <sulzynski.olivier@gmail.com>
2022-11-23 12:49:31 +0100

    BETA V0.51

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,23 @@ function scrollFunction() {
     document.getElementById("sb1-idimglogo").style.height = "150px";
   }
 }
+
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
