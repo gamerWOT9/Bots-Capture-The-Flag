2a3f1881ad7c5f697749fcef181ebe1306a4a561
gamerWOT9 <sulzynski.olivier@gmail.com>
2022-12-13 20:29:18 +0100

    BETA V0.64

--- static/js/script.js
+++ static/js/script.js
@@ -40,3 +40,29 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+
+
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+  var sb1 = document.getElementById("sb1id-links")
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "flex";
+  sb1.style.height = "160px";
+  evt.currentTarget.className += " active";
+}
